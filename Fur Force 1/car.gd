a46c6258ba8e5290c813387dc83a0f872fc11429
Kosina Zoltán <kosinaz@gmail.com>
2025-07-05 10:08:19 +0200

    speed fix

--- Fur Force 1/car.gd
+++ Fur Force 1/car.gd
@@ -13,24 +13,24 @@ var right_lane = true
 var action_pressed = false
 var arrived = false
 
-func _process(_delta):
+func _process(delta):
 	if arrived:
 		return
-	position += Vector2(3, 1.5)
+	position += Vector2(3, 1.5) * delta * 50
 	if state == states.MOVING_FORWARD:
 		if action_pressed:
 			state = states.CHANGING_LANES
 			action_pressed = false
 	elif state == states.CHANGING_LANES:
 		if right_lane:
-			$Sprite.position.x += 1
+			$Sprite.position.x += 1 * delta * 100
 			if $Sprite.position.x > 30:
 				$Sprite.position.x = 30
 				state = states.MOVING_FORWARD
 				right_lane = false
 				emit_signal("changed_lane")
 		else:
-			$Sprite.position.x -= 1
+			$Sprite.position.x -= 1 * delta * 100
 			if $Sprite.position.x < 0:
 				$Sprite.position.x = 0
 				state = states.MOVING_FORWARD
